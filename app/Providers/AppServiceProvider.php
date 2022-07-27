183f0f2682de65f48ddb23782669b716e8a4f9d8
voidnowhere <alivnw@outlook.com>
2022-07-27 23:03:40 +0100

    set url scheme to https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Models\User;
 use App\Services\MailchimpNewsletter;
 use App\Services\NewsletterInterface;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use MailchimpMarketing\ApiClient;
 
@@ -36,6 +38,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::define('admin', fn(User $user) => $user->is_admin);
 
         Blade::if('admin', fn() => request()->user()?->can('admin'));
